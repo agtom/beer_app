9ff84f09c977384d29f865224c3fa2ffe1c9e4cf
Andrew Tomlinson <andrewtomlinson@Andrews-MacBook-Air.local>
2015-06-02 12:04:21 -0400

    migration

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,6 @@
 class Beer < ActiveRecord::Base
   validates :name, presence: true
   validates_length_of :description, :minimum => 10, :maximum => 50
+
+  belongs_to :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: {message: "I know you"}
+
+  has_many :beers
+
 end
